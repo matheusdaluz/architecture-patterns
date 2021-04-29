6ede0183aaabe4bc154511b4960a1fcea177c845
matheus da luz <matheusdaluzcosta@gmail.com>
2021-04-29 18:19:23 -0300

    tdd

---
+++ model.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import Optional, NewType
+from datetime import date
+
+Quantity = NewType("Quantity", int)
+Sku = NewType("Sku", str)
+Reference = NewType("Reference", str)
+
+@dataclass(frozen=True)
+class OrderLine:
+    orderid: str
+    sku: Sku
+    qty: Quantity
+
+
+class Batch:
+    def __init__(self, ref: Reference, sku: Reference, qty: Quantity, eta: Optional[date]):
+        self.reference = ref
+        self.sku = sku
+        self.eta = eta
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
+
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def deallocate(self, line: OrderLine) -> bool:
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+        
