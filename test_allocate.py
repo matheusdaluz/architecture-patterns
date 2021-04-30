ebf9e306ac208d10467660288eb630a78a6121fc
matheus.costa <matheus.costa@g.globo>
2021-04-29 21:22:42 -0300

    rubbish deleted

--- test_allocate.py
+++ test_allocate.py
@@ -1,11 +1,15 @@
+from datetime import date, timedelta
 from model import OrderLine, Batch
 
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = tomorrow + timedelta(days=10)
 
 def test_prefers_current_stock_batches_to_shipments():
     in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
     shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
     line = OrderLine("oref", "RETRO-CLOCK", 10)
-    
+
     allocate(line, [in_stock_batch, shipment_batch])
 
     assert in_stock_batch.available_quantity == 90
@@ -26,11 +30,12 @@ def test_prefers_earlier_batches():
 
 
 def test_returns_allocated_batch_ref():
-    in_stock_batch = Batch("in-stock-batch-ref", "HIGHBROW-POSTER", 100, eta=None)
-    shipment_batch = Batch("shipment-batch-ref", "HIGHBROW-POSTER", 100, eta=tomorrow)
+    in_stock_batch = Batch("in-stock-batch-ref",
+                           "HIGHBROW-POSTER", 100, eta=None)
+    shipment_batch = Batch("shipment-batch-ref",
+                           "HIGHBROW-POSTER", 100, eta=tomorrow)
 
     line = OrderLine("oref", "HIGHBROW-POSTER", 10)
 
     allocation = allocate(line, [in_stock_batch, shipment_batch])
     assert allocation == in_stock_batch
-
