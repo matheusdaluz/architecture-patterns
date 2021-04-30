8c62aea39c998817e449c55985571021b438a1a8
matheus da luz <matheusdaluzcosta@gmail.com>
2021-04-30 07:46:17 -0300

    first domain service

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
-from typing import Optional, NewType
+from typing import Optional, NewType, List
 from datetime import date
 
 Quantity = NewType("Quantity", int)
 Sku = NewType("Sku", str)
 Reference = NewType("Reference", str)
+
 
 @dataclass(frozen=True)
 class OrderLine:
@@ -39,11 +40,35 @@ class Batch:
     @property
     def available_quantity(self) -> int:
         return self._purchased_quantity - self.allocated_quantity
-    
+
     def __eq__(self, other):
         if not isinstance(other, Batch):
             return False
         return other.reference == self.reference
-    
+
     def __hash__(self):
         return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(
+            b for b in sorted(batches) if b.can_allocate(line)
+        )
+
+        batch.allocate(line)
+    except StopIteration:
+        raise OutOfStock(f'Out of stock for sku {line.sku}')
+    
+    return batch.reference
+
+
+class OutOfStock(Exception):
+    pass
--- test_allocate.py
+++ test_allocate.py
@@ -1,9 +1,11 @@
+import pytest
 from datetime import date, timedelta
-from model import OrderLine, Batch
+from model import OrderLine, Batch, allocate, OutOfStock
 
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
+
 
 def test_prefers_current_stock_batches_to_shipments():
     in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
@@ -38,4 +40,12 @@ def test_returns_allocated_batch_ref():
     line = OrderLine("oref", "HIGHBROW-POSTER", 10)
 
     allocation = allocate(line, [in_stock_batch, shipment_batch])
-    assert allocation == in_stock_batch
+    assert allocation == in_stock_batch.reference
+
+
+def test_raises_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch("batch1", "SMALL-FORk", 10, eta=today)
+    allocate(OrderLine("order1", "SMALL-FORk", 10), [batch])
+
+    with pytest.raises(OutOfStock, match="SMALL-FORK"):
+        allocate(OrderLine("order2", "SMALL-FORK", 10), [batch])
